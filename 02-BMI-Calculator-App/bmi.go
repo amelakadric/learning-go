7bbdea2379cc00e5ff01a3966176a1c3e8e8b558
amelakadric <amela00108@gmail.com>
2022-07-13 03:27:50 +0200

    splitting code into files and packages

--- 02-BMI-Calculator-App/bmi.go
+++ 02-BMI-Calculator-App/bmi.go
@@ -1,42 +1,44 @@
 package main
 
 import (
-	"bufio"
 	"fmt"
-	"os"
+	"strconv"
 	"strings"
+
+	"github.com/learning-go/02-BMI-Calculator-App/info"
 )
 
-var reader = bufio.NewReader(os.Stdin)
+
 
 func main() {
 
 	//Output info
-	fmt.Println("BMI Calculator")
-	fmt.Println("-----------------")
+	fmt.Println(info.MainTitle)
+	fmt.Println(info.Separator)
 
 	//Prompt for user input (weight + height)
-	fmt.Print("Please enter your weight (kg): ")
+	fmt.Print(info.WeightPrompt)
 	weightInput, _ := reader.ReadString('\n')
 
 
-	fmt.Print("Please enter your height (m): ")
+	fmt.Print(info.HeightPrompt)
 	heightInput, _ := reader.ReadString('\n')
 	
 	//it includes the \n from input
 	//cleaning the input
 
+	// Save that user input in variables
+
 	weightInput = strings.Replace(weightInput, "\r\n", "", -1)
 	heightInput = strings.Replace(heightInput, "\r\n", "", -1)
 
-	fmt.Print(weightInput)
-
-	fmt.Print(heightInput)
-
-
-	// Save that user input in variables
+	weight, _:= strconv.ParseFloat(weightInput, 64);
+	height, _:= strconv.ParseFloat(heightInput, 64);
 
 	//Calculate the BMI (weight / (height * height))
+	bmi:= weight/(height*height)
+
 
 	//Output the calculated BMI
+	fmt.Printf("Your BMI: %.2f", bmi)
 }
