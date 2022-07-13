e46ea7ce57e0ceebf6daa63fe54c85d979da5d4d
amelakadric <amela00108@gmail.com>
2022-07-13 16:29:11 +0200

    splitting code using functions

--- 02-BMI-Calculator-App/bmi.go
+++ 02-BMI-Calculator-App/bmi.go
@@ -1,44 +1,21 @@
 package main
 
 import (
-	"fmt"
-	"strconv"
-	"strings"
-
 	"github.com/learning-go/02-BMI-Calculator-App/info"
 )
 
-
-
 func main() {
 
-	//Output info
-	fmt.Println(info.MainTitle)
-	fmt.Println(info.Separator)
+	info.PrintWelcome()
 
-	//Prompt for user input (weight + height)
-	fmt.Print(info.WeightPrompt)
-	weightInput, _ := reader.ReadString('\n')
+	weight, height := getUserMetrics()
 
+	bmi:=calculateBMI(weight, height)
 
-	fmt.Print(info.HeightPrompt)
-	heightInput, _ := reader.ReadString('\n')
-	
-	//it includes the \n from input
-	//cleaning the input
+	printBMI(bmi)
 
-	// Save that user input in variables
+}
 
-	weightInput = strings.Replace(weightInput, "\r\n", "", -1)
-	heightInput = strings.Replace(heightInput, "\r\n", "", -1)
-
-	weight, _:= strconv.ParseFloat(weightInput, 64);
-	height, _:= strconv.ParseFloat(heightInput, 64);
-
-	//Calculate the BMI (weight / (height * height))
-	bmi:= weight/(height*height)
-
-
-	//Output the calculated BMI
-	fmt.Printf("Your BMI: %.2f", bmi)
+func calculateBMI(weight float64, height float64) float64{
+	return weight/(height*height)
 }
--- 02-BMI-Calculator-App/input.go
+++ 02-BMI-Calculator-App/input.go
@@ -2,7 +2,32 @@ package main
 
 import (
 	"bufio"
+	"fmt"
 	"os"
+	"strconv"
+	"strings"
+
+	"github.com/learning-go/02-BMI-Calculator-App/info"
 )
 
 var reader = bufio.NewReader(os.Stdin)
+
+func getUserMetrics()(weight float64, height float64){
+
+	weight = getUserInput(info.WeightPrompt)
+	//it includes the \n from input
+	//cleaning the input
+	
+	height = getUserInput(info.HeightPrompt)
+
+	return 
+}
+
+func getUserInput(promptText string) (value float64){
+	fmt.Print(promptText)
+
+	userInput, _ := reader.ReadString('\n')
+	userInput = strings.Replace(userInput, "\r\n", "", -1)
+	value, _= strconv.ParseFloat(userInput, 64);
+	return 
+}
