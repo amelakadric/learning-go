ae85fc0f78f8f405ea6197de218dc0c0fe190bf8
amelakadric <amela00108@gmail.com>
2022-07-14 14:06:16 +0200

    creating struct instances

--- 05-structs/structs.go
+++ 05-structs/structs.go
@@ -9,24 +9,40 @@ import (
 	"time"
 )
 
+type User struct {
+	firstName   string
+	lastName    string
+	birthDate   string
+	createdDate time.Time
+}
+
 var reader = bufio.NewReader(os.Stdin)
 
 func main() {
+
+	var newUser User
+
 	firstName := getUserData("Please enter your first name: ")
 	lastName := getUserData("Please enter your last name: ")
 	birthdate := getUserData("Please enter your birthdate (MM/DD/YYYY): ")
-	created:= time.Now()
+	created := time.Now()
+
+	newUser = User{
+		firstName:   firstName,
+		lastName:    lastName,
+		birthDate:   birthdate,
+		createdDate: created}
 
 	// ... do something awesome with that gathered data!
 
-	fmt.Println(firstName, lastName, birthdate, created)
+	fmt.Println(newUser)
 }
 
 func getUserData(promptText string) string {
 	fmt.Print(promptText)
 	userInput, _ := reader.ReadString('\n')
 
-	var cleanedInput string;
+	var cleanedInput string
 
 	if runtime.GOOS == "windows" {
 		cleanedInput = strings.Replace(userInput, "\r\n", "", -1)
