c97431797bb3d4ce77ccb2e06cba265960b43166
amelakadric <amela00108@gmail.com>
2022-07-14 02:34:59 +0200

    advantages of pointers

--- 04-pointers/pointers.go
+++ 04-pointers/pointers.go
@@ -1,6 +1,8 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+)
 
 func main() {
 
@@ -8,8 +10,29 @@ func main() {
 
 	fmt.Println(age)
 
-	myAge := &age
+	// myAge := &age
+
+	var myAge *int
+	myAge = &age
 
 	fmt.Println(*myAge)
 
+	*myAge = 22
+
+	fmt.Println(*myAge)
+	fmt.Println(age)
+
+	doubledAge := double(myAge)
+	fmt.Println(doubledAge)
+	
+	double(myAge)
+
+	fmt.Println(age)
+}
+
+
+func double(number *int) int {
+	result := *number * 2 // it doesnt make uneccessarry copies of values
+	*number=100
+	return result
 }
