230e2fd3a2f26f29a99dea62d958448944358302
amelakadric <amela00108@gmail.com>
2022-07-17 20:55:51 +0200

    dynamic lists with slices

--- 07-data-collections/lists.go
+++ 07-data-collections/lists.go
@@ -11,35 +11,45 @@ type Product struct {
 }
 
 func main() {
-	var productNames [4]string = [4]string{"A Book"}
-	prices := [4]float64{10.99, 9.99, 45.99, 20.0}
+	prices := []float64{10.99, 8.99}
+	fmt.Println(prices[0:1])
+	prices[1] = 9.99
+
+	prices = append(prices, 5.99)
+	prices = prices[1:]
+
 	fmt.Println(prices)
-
-	productNames[2] = "A Carpet"
-
-	fmt.Println(productNames)
-
-	fmt.Println(prices[3])
-
-	// featuredPrices := prices[1:3] //starts with element 1 excluding element 3
-
-	featuredPrices := prices[1:] //starts at the beginning
-	// featuredPrices := prices[1:]//ends on the end
-
-	featuredPrices[0] = 199.99
-	highlightedPrices := featuredPrices[:1]
-
-	fmt.Println(highlightedPrices)
-	fmt.Println(prices)
-	fmt.Println(len(highlightedPrices), cap(highlightedPrices))
-
-	highlightedPrices = highlightedPrices[:3]
-	fmt.Println(highlightedPrices)
-	fmt.Println(len(highlightedPrices), cap(highlightedPrices))
-
-	//editing slices is editing the array also
-	//beacuse its just like a window into an array
-
-	//https://www.youtube.com/watch?v=j7eEt_jk1V0
-	// F U
 }
+
+// func main() {
+// var productNames [4]string = [4]string{"A Book"}
+// prices := [4]float64{10.99, 9.99, 45.99, 20.0}
+// fmt.Println(prices)
+
+// productNames[2] = "A Carpet"
+
+// fmt.Println(productNames)
+
+// fmt.Println(prices[3])
+
+// // featuredPrices := prices[1:3] //starts with element 1 excluding element 3
+
+// featuredPrices := prices[1:] //starts at the beginning
+// // featuredPrices := prices[1:]//ends on the end
+
+// featuredPrices[0] = 199.99
+// highlightedPrices := featuredPrices[:1]
+
+// fmt.Println(highlightedPrices)
+// fmt.Println(prices)
+// fmt.Println(len(highlightedPrices), cap(highlightedPrices))
+
+// highlightedPrices = highlightedPrices[:3]
+// fmt.Println(highlightedPrices)
+// fmt.Println(len(highlightedPrices), cap(highlightedPrices))
+// //editing slices is editing the array also
+// //beacuse its just like a window into an array
+//
+//https://www.youtube.com/watch?v=j7eEt_jk1V0
+// F U
+// }
