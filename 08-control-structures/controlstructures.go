8f744750fd674c6bac69da8ee6ee9aa39e7d564d
amelakadric <amela00108@gmail.com>
2022-07-19 00:57:00 +0200

    else and if else

--- 08-control-structures/controlstructures.go
+++ 08-control-structures/controlstructures.go
@@ -16,8 +16,16 @@ func main() {
 	userAgeInput = strings.Replace(userAgeInput, "\r\n", "", -1)
 	userAge, _ := strconv.ParseInt(userAgeInput, 0, 64)
 
-	if userAge >= 18 {
+	// isOldEnough := userAge >=18
+
+	if (userAge >= 30 && userAge < 50) || userAge >= 60 {
+		fmt.Println("You're eligible for our senior jobs")
+	} else if userAge >= 50 {
+		fmt.Println("The best age")
+	} else if userAge >= 18 {
 		fmt.Println("Welcome to the club")
+	} else {
+		fmt.Println("Sorry, you're not old enough :/")
 	}
 
 	fmt.Println("Goodbye")
