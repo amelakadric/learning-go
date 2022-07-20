6684e52c4bdee3c0e0295247980cd117684deb3b
amelakadric <amela00108@gmail.com>
2022-07-20 14:28:00 +0200

    adding core game steps and logic + starting game function

--- 09-monster-slayer-game/main.go
+++ 09-monster-slayer-game/main.go
@@ -1,5 +1,27 @@
 package main
 
+import "github.com/amelakadric/learning-go/09-monster-slayer-game/interaction"
+
 func main() {
+	winner := ""
+
+	startGame()
+
+	for winner == "" {
+		winner = executeRound()
+	}
+
+	endGame()
 
 }
+
+func startGame() {
+	interaction.PrintGreeting()
+}
+
+func executeRound() string {
+
+	return ""
+}
+
+func endGame() {}
