fd50caf752ed9a0bdd57329531b3fce8829bf115
amelakadric <amela00108@gmail.com>
2022-07-21 03:07:26 +0200

    fetching user input

---
+++ 09-monster-slayer-game/interaction/input.go
@@ -0,0 +1,26 @@
+package interaction
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strings"
+)
+
+var reader = bufio.NewReader(os.Stdin)
+
+func GetPlayerChoice(specialAttackIsAvailable bool) {
+
+}
+
+func getPlayerInput() (string, error) {
+	fmt.Print("Your choice: ")
+	userInput, err := reader.ReadString('\n')
+
+	if err != nil {
+		return "", err
+	}
+
+	userInput = strings.Replace(userInput, "\r\n", "", -1)
+	return userInput, err
+}
