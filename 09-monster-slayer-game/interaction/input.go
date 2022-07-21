d9cb373449d9951afe5739d0e1109a21fb2054b6
amelakadric <amela00108@gmail.com>
2022-07-21 03:18:04 +0200

    validating and using user input

--- 09-monster-slayer-game/interaction/input.go
+++ 09-monster-slayer-game/interaction/input.go
@@ -9,8 +9,21 @@ import (
 
 var reader = bufio.NewReader(os.Stdin)
 
-func GetPlayerChoice(specialAttackIsAvailable bool) {
+func GetPlayerChoice(specialAttackIsAvailable bool) string {
 
+	for {
+		playerChoice, _ := getPlayerInput()
+
+		if playerChoice == "1" {
+			return "ATTACK"
+		} else if playerChoice == "2" {
+			return "HEAL"
+		} else if playerChoice == "3" && specialAttackIsAvailable {
+			return "SPECIAL_ATTACK"
+		}
+
+		fmt.Println("Fetching the user input failed. Please try again")
+	}
 }
 
 func getPlayerInput() (string, error) {
--- 09-monster-slayer-game/main.go
+++ 09-monster-slayer-game/main.go
@@ -1,6 +1,10 @@
 package main
 
-import "github.com/amelakadric/learning-go/09-monster-slayer-game/interaction"
+import (
+	"fmt"
+
+	"github.com/amelakadric/learning-go/09-monster-slayer-game/interaction"
+)
 
 var currentRound = 0
 
@@ -26,6 +30,9 @@ func executeRound() string {
 	isSpecialRound := (currentRound%3 == 0)
 
 	interaction.ShowAvailableActions(isSpecialRound)
+	userChoice := interaction.GetPlayerChoice(isSpecialRound)
+
+	fmt.Println(userChoice)
 
 	return ""
 }
