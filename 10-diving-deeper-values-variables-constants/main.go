2a302753ddf78daba94e5193efc4a90de84dcb9f
amelakadric <amela00108@gmail.com>
2022-07-24 04:04:17 +0200

    new function

--- 10-diving-deeper-values-variables-constants/main.go
+++ 10-diving-deeper-values-variables-constants/main.go
@@ -1,6 +1,8 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+)
 
 func main() {
 	// new & make
@@ -8,7 +10,23 @@ func main() {
 	// hobbies := []string{"Sports", "Reading"}
 	// composite literal making of a slice
 
+	number := new(int)
+	fmt.Println(number)
+	fmt.Println(*number)
+
+	anotherNumber := 0
+	numberAdress := &anotherNumber
+	fmt.Println(numberAdress)
+	fmt.Println(anotherNumber)
+
 	hobbies := make([]string, 2, 10)
+	moreHobbies := new([]string) //returns a pointer
+	// evenMoreHobbies:= []string{} // make([]string)
+
+	*moreHobbies = append(*moreHobbies, "Swimming")
+	fmt.Println(*moreHobbies)
+
+	// aMap := make(map[string]int, 5)
 
 	hobbies[0] = "Sports"
 	hobbies[1] = "Reading"
