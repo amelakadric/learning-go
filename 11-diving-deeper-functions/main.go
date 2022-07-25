314f0c196058975c7a04ff0c2af31a965d91f1ec
amelakadric <amela00108@gmail.com>
2022-07-25 02:48:46 +0200

    returning functions in functions

--- 11-diving-deeper-functions/main.go
+++ 11-diving-deeper-functions/main.go
@@ -6,10 +6,20 @@ type transformFn func(int) int
 
 func main() {
 	numbers := []int{1, 2, 3, 4}
+	moreNumbers := []int{5, 1, 2}
 	doubled := transformNumbers(&numbers, double)
 	tripled := transformNumbers(&numbers, triple)
 	fmt.Println(doubled)
 	fmt.Println(tripled)
+
+	transformFn1 := getTransformerFunction(&numbers)
+	transformFn2 := getTransformerFunction(&moreNumbers)
+
+	transformedNumbers := transformNumbers(&numbers, transformFn1)
+	moreTransformedNumbers := transformNumbers(&moreNumbers, transformFn2)
+
+	fmt.Println(transformedNumbers)
+	fmt.Println(moreTransformedNumbers)
 }
 
 func transformNumbers(numbers *[]int, transform transformFn) []int {
@@ -18,6 +28,14 @@ func transformNumbers(numbers *[]int, transform transformFn) []int {
 		dNumbers = append(dNumbers, transform(val))
 	}
 	return dNumbers
+}
+
+func getTransformerFunction(numbers *[]int) transformFn {
+	if (*numbers)[0] == 1 {
+		return double
+	} else {
+		return triple
+	}
 }
 
 func double(number int) int {
