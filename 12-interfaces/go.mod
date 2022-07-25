327dbfaed654bc309bbb33edfaa2e9597440460f
amelakadric <amela00108@gmail.com>
2022-07-25 04:22:46 +0200

    interfaces-intro

---
+++ 12-interfaces/go.mod
@@ -0,0 +1,3 @@
+module github.com/amelakadric/learning-go/12-interfaces
+
+go 1.18
---
+++ 12-interfaces/main.go
@@ -0,0 +1,49 @@
+package main
+
+import (
+	"fmt"
+	"io/fs"
+	"io/ioutil"
+)
+
+type logger interface {
+	log()
+}
+
+type logData struct {
+	message  string
+	fileName string
+}
+
+func (lData *logData) log() {
+	err := ioutil.WriteFile(lData.fileName, []byte(lData.message), fs.FileMode(0644))
+
+	if err != nil {
+		fmt.Println("Storing the log data failed!")
+	}
+}
+
+type loggableString string
+
+func (text loggableString) log() {
+	fmt.Println(text)
+}
+
+func main() {
+	userLog := &logData{"User logged in", "user-log.txt"}
+	// do more work ...
+	userLog.log()
+
+	message := loggableString("[INFO] User created!")
+	// do more work ...
+	message.log()
+
+	createLog(userLog)
+
+	createLog(message)
+
+}
+
+func createLog(data logger) {
+	data.log()
+}
