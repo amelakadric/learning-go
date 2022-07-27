e56649c0b14ea04f9ba73ebcc547dff1573f645b
amelakadric <amela00108@gmail.com>
2022-07-28 00:43:36 +0200

    select statement

--- 14-concurrency-goroutines-channels/main.go
+++ 14-concurrency-goroutines-channels/main.go
@@ -10,43 +10,48 @@ var source = rand.NewSource(time.Now().Unix())
 var randN = rand.New(source)
 
 func main() {
-
-	c := make(chan int)
+	x := make(chan int)
+	y := make(chan int)
 	limiter := make(chan int, 3)
 
-	go generateValue(c, limiter)
-	go generateValue(c, limiter)
-	go generateValue(c, limiter)
-	go generateValue(c, limiter)
+	go generateValue(x, limiter)
+	go generateValue(y, limiter)
 
-	sum := 0
-	i := 0
+	var a int
+	var b int
 
-	for num := range c {
-		sum += num
-		i++
-		if i == 4 {
-			close(c)
-		}
+	select {
+	case a = <-x:
+		fmt.Printf("X finished first, value is %v", a)
+	case b = <-y:
+		fmt.Printf("Y finished first, value is %v", b)
 	}
+	// go generateValue(c, limiter)
+	// go generateValue(c, limiter)
 
-	fmt.Println(sum)
+	// sum := 0
+	// i := 0
+
+	// for num := range c {
+	// 	sum += num
+	// 	i++
+	// 	if i == 4 {
+	// 		close(c)
+	// 	}
+	// }
+
+	// fmt.Println(sum)
 }
 
 func generateValue(c chan int, limit chan int) int {
 	limit <- 1
 	fmt.Println("Generating value...")
-	// sleepTime := randN.Intn(3)
-	time.Sleep(time.Duration(4) * time.Second)
+	sleepTime := randN.Intn(3)
+	time.Sleep(time.Duration(sleepTime) * time.Second)
+
 	result := randN.Intn(10)
 	c <- result
 	<-limit
-	//ali ako funkcija ne vraca nista
-	// i samo je goroutine ne crashuje
-	//iako ne zna koliko ce dugo da se
-	//vrti u onom gpre loopu, zanima me sto
-
-	// close(c)
 	return result
 }
 
